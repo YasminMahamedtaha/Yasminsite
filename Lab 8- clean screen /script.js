9f9ce74a219c6a315975f5c6193d3adc46082d45
YasminMahamedtaha <112769449+YasminMahamedtaha@users.noreply.github.com>
2022-09-15 07:26:15 -0500

    Initial Commmit

---
+++ Lab 8- clean screen /script.js
@@ -0,0 +1,66 @@
+
+$(document).ready(function () {
+    "use strict";
+    
+    const $width = $("#width");
+    const $growth = $("#growth");
+    const $rate = $("#rate");
+    const $circlesNumber = $("#circlesNumber");
+
+    
+    $("#startBtn").on("click", function (evt) {
+
+        for (let i = 0; i < parseInt($circlesNumber.val()); i++) {
+            createNewCircle();
+        }
+    });
+
+    
+    $("#circles").on("click", function (event) {
+        createNewCircle();
+    });
+
+    
+    let createNewCircle = function () {
+        const $div = $('<div>', { class: 'circle inlineBlock absolute' });
+        $div.css("background-color", createColor());
+        $("#circles").append($div);
+
+        var xy = getRandomPosition($div);
+        $div.css("top", xy[0] + 'px');
+        $div.css("left", xy[1] + 'px');
+
+        const id = setInterval(function () {
+
+            $($div).fadeOut($rate.val() * 2);
+            const size = parseInt($($div).css("height")) + parseInt($growth.val()) + "px";
+            $($div).css("height", size);
+            $($div).css("width", size);
+            $($div).fadeIn($rate.val() * 2);
+
+        }, parseFloat($rate.val()));
+
+        $div.click(function () {
+            $div.remove();
+            clearInterval(id);
+        });
+    }
+    
+    let getRandomPosition = function (element) {
+        const x = document.body.offsetHeight - element.height();
+        const y = document.body.offsetWidth - element.width();
+        const randomX = Math.floor(Math.random() * x);
+        const randomY = Math.floor(Math.random() * y);
+        return [randomX, randomY];
+    }
+    
+    let createColor = function () {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+});
---
+++ Lab8 w2d2 part2/que4&5.js
@@ -0,0 +1,9 @@
+/*Suppose you are given a file of JavaScript code containing a list of many function and variable
+declarations. All of these function and variable names will be added to the Global JavaScript
+namespace. What simple modification to the JavaScript file can remove all the names from the
+Global namespace?*/
+
+//No 4 Answer: IIFE: declaring and immediately calling an anonymous function at the same time
+// can avoid name space /name collisions and collisions
+
+//Answer question 5
